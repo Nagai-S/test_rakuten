97f5be09f1fdd4dd90c4703ea64963d6110f02e2
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2021-01-21 23:37:23 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Cellular from "expo-cellular";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {Cellular.mobileNetworkCode === "11" ? (
+        <Text>楽天回線です</Text>
+      ) : (
+        <Text>楽天回線ではありません</Text>
+      )}
     </View>
   );
 }
