d1d3ff7333e4f65d47c3cb9933640b1d3f5ad020
Nagai-S <63888620+Nagai-S@users.noreply.github.com>
2021-01-22 01:32:11 +0900

    更新ボタン追加

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as Cellular from "expo-cellular";
 
 export default function App() {
+  const [updateData, setUpdateData]=useState(Date.now())
+  const [mnc, setMnc] = useState(Cellular.mobileNetworkCode);
+
+  useEffect(()=>{
+    setMnc(Cellular.mobileNetworkCode);
+  }, [updateData])
+
+  const onUpdateData=()=>{
+    setUpdateData(Date.now());
+  }
+
   return (
     <View style={styles.container}>
-      {Cellular.mobileNetworkCode === "11" ? (
-        <Text>楽天回線です</Text>
+      {mnc === "11" ? (
+        <Text style={styles.text}>楽天回線です</Text>
       ) : (
-        <Text>楽天回線ではありません</Text>
+        <Text style={styles.text}>楽天回線ではありません</Text>
       )}
+      <TouchableOpacity onPress={onUpdateData} style={styles.button}>
+        <Text style={styles.text}>更新</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,8 +31,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
     justifyContent: 'center',
+  },
+  button: {
+    marginTop: 50,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 40,
   },
 });
